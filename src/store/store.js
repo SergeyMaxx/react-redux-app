32ff37e0e3429a0325a6c00dae1cdb20c82b2a6e
SergeyMaxx <maximov.regen@gmail.com>
2022-06-22 19:15:14 +0300

    change

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import {createStore} from 'redux'
+import {taskReducer} from './task/reduser'
+
+const initialState = [
+  {id: 1, title: 'Task 1', completed: false},
+  {id: 2, title: 'Task 2', completed: false}
+]
+
+const configureStore = () => createStore(taskReducer, initialState)
+
+export default configureStore
