747beb22571d97702c0b852dee55ba173b20e4d7
Sergey Maximov <95900933+SergeyMaxx@users.noreply.github.com>
2023-07-24 14:47:43 +0300

    18. Redux. Часть 1 Задание №1. Удаление задачи

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ReactDOM from 'react-dom/client'
+import * as actions from './store/actions'
+import {initiateStore} from './store/store'
+
+const store = initiateStore()
+
+const App = () => {
+  const [state, setState] = useState(store.getState())
+
+  useEffect(() => {
+    store.subscribe(() => setState(store.getState()))
+  }, [])
+
+  const completeTask = taskId => store.dispatch(actions.taskCompleted(taskId))
+  const changeTitle = taskId => store.dispatch(actions.titleChanged(taskId))
+  const deletedTask = taskId => store.dispatch(actions.taskDeleted(taskId))
+
+  const style = {
+    marginRight: 10,
+    cursor: 'pointer'
+  }
+
+  return (
+    <>
+      <h1> App</h1>
+      <ul>
+        {state.map(el => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Completed: ${el.completed}`}</p>
+            <button style={style} onClick={() => completeTask(el.id)}>
+              Complete
+            </button>
+            <button style={style} onClick={() => changeTitle(el.id)}>
+              Change title
+            </button>
+            <button style={style} onClick={() => deletedTask(el.id)}>
+              Delete task
+            </button>
+            <hr/>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
-    <h1>App</h1>
+    <App/>
   )
